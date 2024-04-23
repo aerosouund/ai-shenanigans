9c6b5395fccc916823b15be5aa98b78ac5e2ed36
aerosouund <aerosound161@gmail.com>
2024-04-23 21:30:47 +0200

    main

--- project-py/main.py
+++ project-py/main.py
@@ -1,9 +1,10 @@
 from pymongo import MongoClient
-from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.vectorstores import MongoDBAtlasVectorSearch
-from langchain.document_loaders import DirectoryLoader
-from langchain.llms import Ollama
+from langchain_community.embeddings import OllamaEmbeddings
+from langchain_community.vectorstores import MongoDBAtlasVectorSearch
+from langchain_community.document_loaders import DirectoryLoader
+from langchain_community.llms import Ollama
 from langchain.chains import RetrievalQA
+import params
 import gradio as gr
 from gradio.themes.base import Base
 import params
@@ -15,17 +16,29 @@ collectionName = "coll"
 collection = client[dbName][collectionName]
 
 
-embdeddings = OpenAIEmbeddings()
+embdeddings = OllamaEmbeddings()
 vector_store = MongoDBAtlasVectorSearch(collection, embdeddings)
 
 def query_data(query):
     docs = vector_store.similarity_search(query, K=1)
     as_output = docs[0].page_content
 
-    llm = Ollama()
+    llm = Ollama(model="llama3")
     retriever = vector_store.as_restriever()
     qa = RetrievalQA.from_chain_type(llm, chain_type="stuff", retriever=retriever)
     retriever_output = qa.run(query)
 
-    return as_output, retriever_output
+    return retriever_output
 
+with gr.Blocks(theme=Base(), title="Ammar demo") as demo:
+    gr.Markdown("Ammar demo")
+    textbox=gr.Textbox(label="enter your question:")
+    with gr.Row():
+        button = gr.Button("submit", variant="primary")
+    with gr.Column():
+        output = gr.Textbox(lines=1, max_lines=10, label="model output")
+
+    button.click(query_data, textbox, outputs=[output])
+
+
+demo.launch()
